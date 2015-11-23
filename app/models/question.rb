e5189a7077f3420e3456769f67cb765f50a11a4a
Kyohei Seo <kyo199333@gmail.com>
2015-11-23 16:03:19 +0900

    make not see answers , case of user does not answer the question

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,18 +7,17 @@ class Question < ActiveRecord::Base
   validates_presence_of :user_id, :text, :group_id
 
   after_create :create_feed_content
-  after_update :update_feed_content
 
   def user_answer(user_id)
-    Answer.find_by(user_id: user_id, question_id: id)
+      Answer.find_by(user_id: user_id, question_id: id)
+  end
+
+  def answered?(user)
+    answers.exists?(user_id: user.id)
   end
 
   private
   def create_feed_content
-    self.feed_content = FeedContent.create(group_id: group_id, updated_at: updated_at)
-  end
-
-  def update_feed_content
-    self.feed_content.update(group_id: group_id, updated_at: updated_at)
+    self.feed_content = FeedContent.create(group_id: group_id, created_at: created_at)
   end
 end
